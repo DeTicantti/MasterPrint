459a4025891aebfc9eea25cf7d0220d87d8aa6b9
DeTicantti <cr.deticantti@hotmail.com>
2022-01-25 17:26:45 -0600

    numero de ticket

--- main/db/rep/ticket.php
+++ main/db/rep/ticket.php
@@ -9,6 +9,7 @@ if (isset($_POST['ticket'])){
     $timeobj = new DateTimeZone('America/Mexico_City');
     $timeobjeto = new DateTime('now', $timeobj);
     $timetrab = $timeobjeto->format('j/m/y');
+    $day = $timeobjeto->format('m-j-y');
 
 /*
 SELECT AUTO_INCREMENT - 1 as CurrentId FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'masterprint' AND TABLE_NAME = 'reparaciones'
@@ -19,6 +20,8 @@ $rep->execute();
 $rep->store_result();
 $rep->bind_result($id);
 
+
+
 while($rep->fetch()){
     $sql = "SELECT c.cli_nom, c.cli_ape, c.cli_tel, r.rep_imp, r.rep_mod, r.rep_falla FROM reparaciones r INNER JOIN clientes c ON r.cli_id = c.cli_id WHERE r.rep_id = $id";
 }
@@ -28,11 +31,19 @@ $res->execute();
 $res->store_result();
 $error = array();
 
+
+
 if ($res->num_rows>0) {
     //$res->bind_result($rep_id,$cliente,$impresora,$modelo,$serial,$falla,$status,$tecnico,$diagnostico,$costo,$fechai,$aviso,$garantia,$fechaf);
     $res->bind_result($cliente,$apellido,$telefono,$imp_marca,$imp_modelo,$falla);
 
-    while ($res->fetch()){
+    $sql2 = "SELECT COUNT(*) FROM `reparaciones` WHERE rep_date LIKE '$day%'";
+$rep2 = $con->prepare($sql2);
+$rep2->execute();
+$rep2->store_result();
+$rep2->bind_result($total);
+
+    while ($res->fetch() && $rep2->fetch()){
 
     
 
@@ -40,14 +51,14 @@ if ($res->num_rows>0) {
 
 
 // create new PDF document
-$width = 150;
-$height = 320;
+$width = 150;//150
+$height = 320;//320
 $pageLayout = array($width, $height); //  or array($height, $width) 
 $pdf = new TCPDF('p', 'pt', $pageLayout, true, 'UTF-8', false);
 //$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('Carlos');
-$pdf->SetTitle('Ticket #');
+$pdf->SetTitle('Ticket->'.$total);
 $pdf->SetSubject('Masterprint');
 $pdf->SetKeywords('maquina, falla, fecha, hora, cliente');
 
@@ -100,12 +111,12 @@ $pdf->AddPage();
 
 
 $html = <<<EOD
-<i style="margin-top:-10px;"><span  style= "font-size:80px; text-align:right;">222-22-32-60-12</span></i>
+<i style="margin-top:-10px;"><span  style= "font-size:80px; text-align:right;">->>>2-32-60-12</span></i>
 <i style="margin-top:-40px;"><br/>-- cliente: <span  style= "font-size:80px; text-align:right;"><br/> $cliente $apellido <br>$telefono</span></i><br/>
 <i style="float:left;">-- Equipo:<span  style= "font-size:80px; text-align:right;"><br/> $imp_marca $imp_modelo </span></i><br/>
 <i style ="text-align:right;">----$timetrab---</i>
 <i style="margin-top:-40px;"><br/>-- falla: <span  style= "font-size:80px; text-align:right;"><br/> $falla</span></i>
-<i><h6 style= "text-align:right; font-size:60px;width:80px">****Estimado cliente, le informamos: --- ---tiene un lapso de 15 dias para ------retirar su equipo a partir de la ----fecha acordada, presente esta -----hoja, sin ella no se hará entrega ----de su equipo, pasando este periodo --el taller no se resposabilizara de los equipos olvidados</h6></i>
+<i><h6 style= "text-align:right; font-size:60px;width:80px">****Estimado cliente, le informamos: --- ---tiene un lapso de 15 dias para ------retirar su equipo a partir de la ----fecha acordada, presente esta -----hoja, sin ella no se hará entrega ----de su equipo, pasando este periodo --el taller no se resposabilizara de los equipos olvidados</h6></i><i style ="text-align:right;">----$total---</i>
 EOD;
 
 // Print text using writeHTMLCell()
